43ed0acbb6284614e7be4eb6917c45672a1157cf
Equ17y <antonsobol28@gmail.com>
2025-11-27 01:43:46 +0300

    создана модель Guest

--- meetup_core/models/Guest.py
+++ meetup_core/models/Guest.py
@@ -1,1 +1,14 @@
 # Гость мероприятия и всё что будет связано с ним
+from django.db import models
+
+
+class Guest(models.Model):
+    telegram_id = models.IntegerField(unique=True, verbose_name="ID в Telegram")
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+    phone = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name or ''} {self.last_name or ''} ({self.telegram_id})"
