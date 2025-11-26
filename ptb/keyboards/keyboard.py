408affdcb9afa6ad1914828e778d8f139f8b5af9
Equ17y <antonsobol28@gmail.com>
2025-11-27 01:44:50 +0300

    создал клавиатуры для интерфейса гостя

--- ptb/keyboards/keyboard.py
+++ ptb/keyboards/keyboard.py
@@ -5,3 +5,19 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 test_menu = InlineKeyboardMarkup([
     [InlineKeyboardButton('тест', callback_data='test')]
 ])
+
+def guest_keyboard():
+    """
+    Основная клавиатура для гостя (слушателя)
+    Содержит основные функции доступные гостю:
+    - Просмотр программы мероприятий
+    - Просмотр предстоящих мероприятий
+    - Просмотр своего профиля
+    """
+    keyboard = [
+        [
+            InlineKeyboardButton('Программа мероприятий', callback_data='program'),
+            InlineKeyboardButton('Предстоящие мероприятия', callback_data='upcoming')
+        ]
+    ]
+    return InlineKeyboardMarkup(keyboard)
