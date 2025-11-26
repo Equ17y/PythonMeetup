2193cd8d974d077651a30ae62f0af480c7987a9a
Equ17y <antonsobol28@gmail.com>
2025-11-27 01:45:45 +0300

    главный conversation handler для управления состояниями

--- ptb/handlers/conversation_handlers.py
+++ ptb/handlers/conversation_handlers.py
@@ -3,17 +3,17 @@ from telegram.ext import(
     CommandHandler,
     CallbackQueryHandler,
     ConversationHandler,
-    filters,
 )
-from . import callback_handlers, cmd_handlers, states_bot
+from . import cmd_handlers, states_bot
 
+from .callback_handlers import guest_handler_main_menu
 
 conversation_handler = ConversationHandler(
-    entry_points=[CommandHandler('start', cmd_handlers.start)],
+    entry_points=[CommandHandler('start', cmd_handlers.guest_start)],
     states={
         states_bot.TEST: [
-            CallbackQueryHandler(callback_handlers.handler_main_menu)
+            CallbackQueryHandler(guest_handler_main_menu),
         ],
     },
-    fallbacks=[CommandHandler('start', cmd_handlers.start)]
+    fallbacks=[CommandHandler('start', cmd_handlers.guest_start)]
 )
