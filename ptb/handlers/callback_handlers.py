83b1a9caf2df1d8c0675e2c11962bbb0f1b7ea14
Equ17y <antonsobol28@gmail.com>
2025-11-27 01:45:23 +0300

    обработчики callback'ов для кнопок гостя

--- ptb/handlers/callback_handlers.py
+++ ptb/handlers/callback_handlers.py
@@ -1,14 +1,32 @@
 # Основные обработчики
 from . import states_bot
 from ptb.keyboards import keyboard
+from asgiref.sync import sync_to_async
 
+async def guest_handler_main_menu(update, context):
+    """
+    Обработчик callback'ов для гостя
+    Обрабатывает нажатия на кнопки клавиатуры гостя
+    """
+    from meetup_core.models.Guest import Guest
 
-async def handler_main_menu(update, context):
     query = update.callback_query
     await query.answer()
 
-    await query.edit_message_text(
-        text='Главное меню',
-        reply_markup=keyboard.test_menu,
-    )
+    callback_data = query.data
+
+    if callback_data == 'program':
+        await query.edit_message_text(
+            "Программа мероприятий",
+            reply_markup=keyboard.guest_keyboard()
+        )
+
+    elif callback_data == 'upcoming':
+        await query.edit_message_text(
+            "Предстоящие мероприятия",
+            reply_markup=keyboard.guest_keyboard()
+
+        )
+
     return states_bot.TEST
+
