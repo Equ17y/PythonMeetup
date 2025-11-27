4ff825070eaab4149ba253e00aad5aa9d4e290b4
Equ17y <antonsobol28@gmail.com>
2025-11-28 02:17:35 +0300

    Connect broadcast handlers to main

--- ptb/handlers/conversation_handlers.py
+++ ptb/handlers/conversation_handlers.py
@@ -3,9 +3,12 @@ from telegram.ext import(
     CommandHandler,
     CallbackQueryHandler,
     ConversationHandler,
+    MessageHandler,
+    filters
 )
 from . import cmd_handlers, states_bot
 from .callback_handlers import main_menu_handler, events_list_handler
+from .broadcast_handlers import receive_broadcast_text, confirm_broadcast
 
 conversation_handler = ConversationHandler(
     entry_points=[
@@ -20,6 +23,18 @@ conversation_handler = ConversationHandler(
         ],
         states_bot.EVENT_PROGRAM: [
             CallbackQueryHandler(events_list_handler),
+        ],
+        states_bot.BROADCAST_TEXT: [
+            MessageHandler(
+                filters.TEXT & ~filters.COMMAND,
+                receive_broadcast_text
+            )
+        ],
+        states_bot.BROADCAST_CONFIRM: [
+            MessageHandler(
+                filters.TEXT & ~filters.COMMAND,
+                confirm_broadcast
+            )
         ],
     },
     fallbacks=[CommandHandler('start', cmd_handlers.start)]
